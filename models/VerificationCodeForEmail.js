99cb80c336cdadecf18139831e24ddbbf8fb02e1
nikitabaladha <nikitabaladha1996@gmail.com>
2025-05-27 14:49:16 +0530

    All code taken from Edprowise-Backend repo

---
+++ models/VerificationCodeForEmail.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+const VerificationCodeForEmailSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  code: { type: String, required: true },
+  expiresAt: { type: Date, required: true }, // Expiry time
+});
+
+export default mongoose.model("VerificationCodeForEmail", VerificationCodeForEmailSchema);
